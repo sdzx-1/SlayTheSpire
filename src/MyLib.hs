885bf519dd8bcdfac6ad6626cee199c505e2d23c
sdzx-1 <yangm@emqx.io>
2022-11-27 14:48:41 +0800

    add Module Buff

--- src/MyLib.hs
+++ src/MyLib.hs
@@ -20,119 +20,37 @@ import qualified Data.Map as Map
 import Data.Maybe (fromJust)
 import System.Random (mkStdGen)
 
+import Buff
 import Control.Carrier.Fresh.Strict (runFresh)
-import Control.Effect.Optics (assign, modifying, use)
+import Control.Effect.Optics (modifying, use)
 import qualified Data.IntMap as IntMap
 import Game.Buff
 import Game.Function
-import Game.HList
-import Game.Input
 import Game.Trigger
 import Game.Type
 import Game.VarMap
 
+initEnemys :: IntMap.IntMap Enemy
 initEnemys =
   IntMap.fromList
     [ (i, Enemy 10 10 10)
     | i <- [0 .. 10]
     ]
 
-runF =
-  runLabelledLift
-    . runState (Player 100 0 1)
-    . runState (Game 0 initEnemys)
-    . runState (VarMap Map.empty)
-    . runState (BuffMap Map.empty)
-    . runState (TriggerMap IntMap.empty)
-    . runFresh 0
-    . runRandom (mkStdGen 10)
-    . runError @GameError
-    $ f
-      [ PBuff
-          { buffName = BuffName "Converts enemy attacks to damage 30 times"
-          , buffDefined =
-              definedBuff @'[PlayerTakesDamage]
-                BuffDesc
-                  { defaultVarValues = 0 :* 0 :* VNil
-                  , triggerFunsDefined = \(times :* totalDamageInc :* _) ->
-                      BuffBehave
-                        { triggerFunList =
-                            PriorityAndTriggerFun
-                              { priority = 0
-                              , triggerFun = \SPlayerTakesDamage{enemyAttack} -> Action $ withMaxTriggerTimes 30 $ do
-                                  modifyVar times (+ 1)
-                                  modifyVar totalDamageInc (+ enemyAttack)
-                                  lift $ putStrLn "Converts enemy attacks to damage, interrupt enemy attack"
-                                  modifying @Player #damage (+ enemyAttack)
-                                  throwError InterruptAttack
-                              }
-                              ::: HNil
-                        , description = Desciption $ do
-                            times' <- useVar times
-                            tdi' <- useVar totalDamageInc
-                            pure $
-                              "now times: [" ++ show times' ++ "], " ++ "total damage inc: [" ++ show tdi' ++ "]"
-                        }
-                  }
-          }
-      , PBuff
-          { buffName = BuffName "The player can revive 10 times after death"
-          , buffDefined =
-              definedBuff @'[PlayerDies]
-                BuffDesc
-                  { defaultVarValues = 0 :* VNil
-                  , triggerFunsDefined = \(times :* _) ->
-                      BuffBehave
-                        { triggerFunList =
-                            PriorityAndTriggerFun
-                              { priority = 0
-                              , triggerFun = \SPlayerDies -> Action $ withMaxTriggerTimes 10 $ do
-                                  modifyVar times (+ 1)
-                                  lift $ putStrLn "player dies, revive, select health"
-                                  let healthList = Avi "SELECT HEALTH" [(a, b, show b) | a <- [1 .. 7], let b = a * 10]
-                                  res' <- getInput (healthList :- N)
-                                  case res' of
-                                    Nothing -> assign @Player #health 100
-                                    Just (I h :- N) -> assign @Player #health h
-                              }
-                              ::: HNil
-                        , description = Desciption $ do
-                            times' <- useVar times
-                            pure $ "player dies times: [" ++ show times' ++ "]"
-                        }
-                  }
-          }
-      , PBuff
-          { buffName = BuffName "remain attack to select new enemy killed"
-          , buffDefined =
-              definedBuff @'[EnemyDies, NewTurnStart]
-                BuffDesc
-                  { defaultVarValues = 0 :* VNil
-                  , triggerFunsDefined = \(turnV :* _) ->
-                      BuffBehave
-                        { triggerFunList =
-                            PriorityAndTriggerFun
-                              { priority = 0
-                              , triggerFun = \SEnemyDies{remainAttack} -> Action $ do
-                                  val <- useVar turnV
-                                  lift $ putStrLn $ "buff: remainAttack " ++ show remainAttack ++ " + trun add " ++ show val
-                                  randomSelectEnemyAttack (remainAttack + val)
-                              }
-                              ::: PriorityAndTriggerFun
-                                { priority = 0
-                                , triggerFun = \SNewTurnStart -> Action $ do
-                                    modifyVar turnV (+ 1)
-                                    val <- useVar turnV
-                                    lift $ putStrLn $ "new turn inc damage: " ++ show val
-                                }
-                              ::: HNil
-                        , description = Desciption $ do
-                            v <- useVar turnV
-                            pure $ "add damage [" ++ show v ++ "]"
-                        }
-                  }
-          }
-      ]
+runF :: IO ()
+runF = do
+  res <-
+    runLabelledLift
+      . runState (Player 100 0 1)
+      . runState (Game 0 initEnemys)
+      . runState (VarMap Map.empty)
+      . runState (BuffMap Map.empty)
+      . runState (TriggerMap IntMap.empty)
+      . runFresh 0
+      . runRandom (mkStdGen 10)
+      . runError @GameError
+      $ f [absorbDamage, resurrectThePlayer, moreDamage]
+  print res
 f
   :: forall sig m
    . All sig m
